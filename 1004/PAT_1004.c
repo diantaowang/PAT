548e6d27165878e731ae09443308a82f64749171
dtwang <415376530@qq.com>
2017-10-29 22:24:29 +0800

    half.

--- 1004/PAT_1004.c
+++ 1004/PAT_1004.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
+#include <malloc.h>
+
+typedef struct _TreeNode *PtrToNode;
+
+typedef struct _TreeNode{
+	PtrToNode FirstChild;
+	PtrToNode NextSibling;
+} TreeNode;
 
 int main()
 {
-
+	int m, n;
+	PtrToNode Ptr;
+	scanf("%d%d", &m, &n);
+	Ptr = (PtrToNode)malloc(sizeof(TreeNode));
+	
 
 	return 0;
 }
