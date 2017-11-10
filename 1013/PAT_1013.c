e70327a2ab5a4817e738ff60185cbe2d52cbd63f
dtwang <415376530@qq.com>
2017-11-11 00:49:59 +0800

    Battle Over Cities.

--- 1013/PAT_1013.c
+++ 1013/PAT_1013.c
@@ -1,88 +1,59 @@
 #include <stdio.h>
 #include <malloc.h>
 
-#define MAX_VERTEX 80
+#define MAX_VERTEX 1001
 
-typedef struct _Graph{
+typedef struct _Graph {
 	int vertexs[MAX_VERTEX];
 	int AdjMatrix[MAX_VERTEX][MAX_VERTEX];
 } Graph;
 
-int Visited[MAX_VERTEX] = {0};
+int N, Visited[MAX_VERTEX];
+Graph G;
 
-void DFS_VISIT(Graph G, int i)
+void DFS_VISIT(int i)
 {
 	Visited[i] = 1;
-	for(int j=0; j<MAX_VERTEX-1; j++)
-	{
-		if(Visited[j] == 0 && G.AdjMatrix[i][j] == 1)
-		{
-//			printf("JJJJJJJJJJJJJJJ:  %d\n", j);
-			DFS_VISIT(G, j);
-		}
+	for (int j = 0; j <= N; j++) {
+		if (Visited[j] == 0 && G.AdjMatrix[i][j] == 1)
+			DFS_VISIT(j);
 	}
 }
 
-void DFS(Graph G, int *R, int index)
+void DFS(int *R)
 {
-	*R = -2;
-	int i;
-	for(i=0; i<MAX_VERTEX-1; i++)
-	{
-		G.AdjMatrix[index][i] = 0;
-		G.AdjMatrix[i][index] = 0;
-	}
-/*	printf("---------------------------\n");
-	for(int j=0; j<MAX_VERTEX-1; j++)
-	{
-		for(int k=0; k<MAX_VERTEX-1; k++)
-			printf("%d ", G.AdjMatrix[j][k]);
-		printf("\n");
-	}*/
-	for(i=0; i<MAX_VERTEX-1; i++)
-		Visited[i] = 0;
-	for(i=0; i<MAX_VERTEX-1; i++)	
-	{
-		if(G.vertexs[i] == 1 && Visited[i] == 0)
-		{
+	*R = -1;
+	for (int i = 0; i <= N; i++) {
+		if (G.vertexs[i] == 1 && Visited[i] == 0) {
 			(*R)++;
-	//		printf("XXXXXXXXXXXX  %d\n", i);
-			DFS_VISIT(G, i);
+			DFS_VISIT(i);
 		}
 	}
 }
 
 int main()
 {
-	int N, M, K;
-	int i, j, t1, t2, index;
+	int M, K, *R;
+	int i, j, t1, t2;
 	int Losted[MAX_VERTEX];
-	int *R;
-	Graph G = {{0},{0}};
 	scanf("%d%d%d", &N, &M, &K);
-	R = (int*)malloc(sizeof(int)*K);
-	for(i=0; i<M; i++)
-	{
+	R = (int *)malloc(sizeof(int) * K);
+	for (i = 1; i <= N; i++)
+		G.vertexs[i] = 1;
+	for (i = 0; i < M; i++) {
 		scanf("%d%d", &t1, &t2);
-		G.vertexs[t1] = 1;
-		G.vertexs[t2] = 1;
 		G.AdjMatrix[t1][t2] = 1;
 		G.AdjMatrix[t2][t1] = 1;
 	}
-	for(i=0; i<K; i++)
+	for (i = 0; i < K; i++)
 		scanf("%d", &Losted[i]);
-	/*for(i=0; i<MAX_VERTEX-1; i++)
-	{
-		for(j=0; j<MAX_VERTEX-1; j++)
-			printf("%d ", G.AdjMatrix[i][j]);
-		printf("\n");
-	}*/
-	for(i=0; i<K; i++)
-	{
-		index = Losted[i];
-		DFS(G, R+i, index);
+	for (i = 0; i < K; i++) {
+		for (j = 0; j <= N; j++)
+			Visited[j] = 0;
+		Visited[Losted[i]] = 1;
+		DFS(R + i);
 	}
-	for(i=0; i<K; i++)
-		printf("%d\n", *(R+i));
+	for (i = 0; i < K; i++)
+		printf("%d\n", *(R + i));
 	return 0;
 }
