0d522a2e83a1cc0f502929c051fa71982119a61e
dtwang <415376530@qq.com>
2017-11-18 22:33:42 +0800

    Count PAT's.

--- 1093/PAT_1093_old.c
+++ 1093/PAT_1093_old.c
@@ -5,7 +5,8 @@ int main()
 {
 	char S[100001];
 	int PA[100001];
-	int num = 0, N, i, count = 0;
+	int *AT;
+	int num = 0, N, n, i, count = 0;
 	long long int sum = 0;
 	scanf("%s", S);
 	for (i = 0; i < 100001 && S[i] != '\0'; i++) {
@@ -16,12 +17,16 @@ int main()
 	}
 	N = i;
 	num = 0;
+	n = count - 1;
+	AT = (int *)malloc(sizeof(int) * count);
 	for (i = N - 1; i >= 0; i--) {
 		if (S[i] == 'A')
-			sum += PA[--count]*num;
+			AT[n--] = num;
 		else if (S[i] == 'T')
 			num++;
 	}
+	for (i = 0; i < count; i++)
+		sum += PA[i] * AT[i];
 	printf("%lld\n", sum % 1000000007);
 	return 0;
 }
