dc75ccd598aafdcbf2c2d022ad112258cdc81d01
dtwang <415376530@qq.com>
2017-11-25 00:17:06 +0800

    General Palindromic Number.

--- 1019/PAT_1019.c
+++ 1019/PAT_1019.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
 
+int NUM[31];
+
+int turn(int N, int b)
+{
+	int i = 0;
+	while (N > 0) {
+		NUM[i++] = N % b;
+		N = N / b;
+	}
+	return i;
+}
+
 int main()
 {
-	int i;	
+	int N, b, count, flag = 0;
+	scanf("%d%d", &N, &b);
+	count = turn(N, b);
+	if (count == 1)
+		printf("Yes\n%d\n", NUM[0]);
+	else {
+		for (int i = 0; i < count / 2; i++) {
+			if (NUM[i] != NUM[count - 1 - i]) {
+				flag = -1;
+				break;
+			}
+		}
+		if (flag == 0)
+			printf("Yes\n");
+		else
+			printf("No\n");
+		for (int i = count - 1; i > 0; i--)
+			printf("%d ", NUM[i]);
+		printf("%d\n", NUM[0]);
+	}
 	return 0;
 }
