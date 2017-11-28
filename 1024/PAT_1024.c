6b01a5ec78cdd8afe6acfa27bd792fc6c35e66d1
dtwang <415376530@qq.com>
2017-11-28 23:41:24 +0800

    tmp

---
+++ 1024/PAT_1024.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+int N;
+
+void creat_new_num(int Num[])
+{
+	int neg[20], c[20];
+	for (int i = 0; i < N; i++) {
+		neg[i] = Num[N - 1 - i];
+	}
+	int t = neg[N - 1] + Num[N - 1];
+	Num[N - 1] = t % 10;
+	c[N - 1] = t / 10;
+	for (int i = N - 2; i >= 0; i--) {
+		t = neg[i] + Num[i] + c[i + 1];
+		Num[i] = t % 10;
+		c[i] = t / 10;
+	}
+	if (c[0] > 0) {
+		for (int i = N; i > 0; i--) {
+			Num[i] = Num[i - 1];
+		}
+		Num[0] = c[0];
+		N++;
+	}
+}
+
+int is_palindromic_num(int Num[])
+{
+	for (int i = 0; i < (N + 1) / 2; i++) {
+		if (Num[i] != Num[N - 1 - i]) {
+			return 0;
+		}
+	}
+	return 1;
+}
+
+int main()
+{
+	long long int result;
+	int K, Num[20], i;
+	char num_tmp[20];
+	scanf("%s%d", num_tmp, &K);
+	for (i = 0; num_tmp[i] != '\0'; i++) {
+		Num[i] = num_tmp[i] - '0';
+	}
+	N = i;
+	for (i = 0; i < K; i++) {
+		creat_new_num(Num);
+		if (is_palindromic_num(Num) == 1 || i == K - 1) {
+			result = Num[N - 1];
+			long long int tmp = 1;
+			for (int j = N - 2; j >= 0; j--) {
+				tmp *= 10;
+				result += tmp * Num[j];
+			}
+			printf("%lld\n%d\n", result, i + 1);
+			return 0;
+		}
+	}
+	return 0;
+}
