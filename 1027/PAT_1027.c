105323f11b03364be6e88ddced4ae0d08a7c71fa
dtwang <415376530@qq.com>
2017-12-03 00:09:18 +0800

    Colors in Mars.

--- 1027/PAT_1027.c
+++ 1027/PAT_1027.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-char tochar[14] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C'};
+char tochar[15] =
+    { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C' };
 
 int main()
 {
-	int A[4], c = 0;
+	int A[4], j = 0;
 	char r[8];
 	scanf("%d%d%d", &A[0], &A[1], &A[2]);
-	for(int i=0; i<3; i++)
-	{
-		r[c++] = tochar[A[i]/13*13];
-		r[c++] = tochar[A[i]%13];
+	for (int i = 0; i < 3; i++) {
+		r[j++] = tochar[A[i] / 13];
+		r[j++] = tochar[A[i] % 13];
 	}
-	printf("%s\n", tochar);
+	printf("#%s\n", r);
 	return 0;
 }
