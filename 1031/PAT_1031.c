f9ff83c1c548b1e5d1ed13d53c7f30892be7ad69
dtwang <415376530@qq.com>
2017-12-05 00:44:32 +0800

    Hello World for U.

--- 1031/PAT_1031.c
+++ 1031/PAT_1031.c
@@ -2,14 +2,13 @@
 
 int main()
 {
-	int N, i, c;
+	int N, i;
 	char s[100];
 	scanf("%s", s);
 	for (i = 0; s[i] != '\0'; i++) ;
 	N = i;
-	c = ((N + 2) - (N + 2) / 3 * 3);
-	int a2 = c + (N + 2) / 3;
 	int a1 = (N + 2) / 3;
+	int a2 = (N + 2) - a1 * 2;
 	for (i = 0; i < (a1 - 1); i++) {
 		printf("%c", s[i]);
 		for (int j = 0; j < a2 - 2; j++) {
